25c01983dfe4f9e8e02fbd8921e395431e2edc43
Alberto Eduardo <alberto.jose.eduardo@gmail.com>
2024-08-01 13:33:47 +0200

    initial commit

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"context"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-lambda-go/lambdacontext"
 )
 
-func handleRequest(ctx lambdacontext.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Hello, World!",
